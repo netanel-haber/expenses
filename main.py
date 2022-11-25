042d3b52255768f8b6b1ce64fe70bda332749837
Netanel Haber <netanel.haber@deci.ai>
2022-11-25 14:17:20 +0200

    cleanup

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
 import pandas as pd
 import column as cl
 import category as ca
-from utils import normalize
+from normalize import normalize
 import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
-    df = normalize(pd.read_csv("balance.csv", thousands=",")).reindex(
-        columns=[cl.ACTION, cl.DATE, cl.DIFF]
-    )
-
-    # by_date_and_action = (
-
-    #     .groupby([df[cl.DATE].dt.year, df[cl.DATE].dt.month])
-    #     .sum(numeric_only=True)
-    # )
+    df = normalize(pd.read_csv("balance.csv", thousands=","))
 
     df.pivot_table(
         index=[df[cl.DATE].dt.year, df[cl.DATE].dt.month],
         columns=cl.ACTION,
         values=cl.DIFF,
-    ).plot(kind="bar", stacked=True)
+    ).plot.bar(stacked=True)
 
-    plt.show(block=False)
+    plt.show()
