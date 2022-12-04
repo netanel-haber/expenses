fc6ff57aaeb7a9318f6ea34befb150c896033e88
Netanel Haber <netanel.haber@deci.ai>
2022-12-04 23:09:19 +0200

    render pivot table in pyscript

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ import column as cl
 import numpy as np
 from normalize import normalize
 import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 
 
-def create_pivot_table(path) -> pd.DataFrame:
+def render_pivot_table(path) -> Figure:
+    fig, ax = plt.subplots()
+    plt.subplots_adjust(bottom=0.2)
+
     df = normalize(pd.read_csv(path, thousands=","))
-    return df.pivot_table(
+    table = df.pivot_table(
         index=[df[cl.DATE].dt.year, df[cl.DATE].dt.month],
         columns=cl.ACTION,
         values=cl.DIFF,
         aggfunc=np.sum,
     )
+    table.plot.bar(stacked=True, ax=ax)
+
+    return fig
 
 
-if __name__ == "__main__":
-    create_pivot_table("balance.csv").plot.bar(stacked=True)
-    plt.show()
+render_pivot_table("balance.csv")
